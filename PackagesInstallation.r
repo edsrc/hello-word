ecf6363323a6ad368aad93492eefe15ec3f11cbb
edsrc <ed.kouadio@gmail.com>
2016-05-24 15:28:23 +0200

    Add files via upload

---
+++ PackagesInstallation.r
@@ -0,0 +1,47 @@
+########## Data manipulation ##########
+install.packages("sqldf", dep=TRUE)
+install.packages("reshape", dep=TRUE)
+install.packages("sampling",dep=TRUE)
+
+install.packages("utils",dep=TRUE)
+#library(R.utils)
+
+########## Mining ##########
+install.packages("rattle", dep=TRUE)
+install.packages("psych", dep=TRUE)
+install.packages("caret",dep=TRUE)
+install.packages("e1071", dep=TRUE)
+install.packages("earth", dep=TRUE)
+install.packages("mda", dep=TRUE)
+install.packages("C50", dep=TRUE)
+install.packages("klaR", dep=TRUE)
+
+install.packages("FactoMineR", dep=TRUE)
+install.packages("conjoint", dep=TRUE)
+
+
+########## Transverses ##########
+install.packages("sas7bdat", dep=TRUE)
+install.packages("foreign", dep=TRUE)
+install.packages("RODBC", dep=TRUE)
+install.packages("XLConnect", dep=TRUE)
+
+########## Pretty ##########
+install.packages("rPython", dep=TRUE)
+install.packages("wordcloud", dep=TRUE)
+
+########## Hide ##########
+install.packages("hash", dep=TRUE)
+install.packages("base64", dep=TRUE)
+install.packages("base64enc", dep=TRUE)
+
+########## unaffected  ##########
+install.packages("Rcmdr", dep=TRUE)
+install.packages("RcmdrPlugin.temis", dep=TRUE)
+install.packages("kohonen", dep=TRUE)
+
+########## Correlation ##########
+install.packages("polycor", dep=TRUE)
+install.packages("corrgram", dep=TRUE)
+
+update.packages()
